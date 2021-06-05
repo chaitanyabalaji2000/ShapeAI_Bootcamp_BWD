13b1c53670f3bdf181ed649109fae00c6593226b
chaitanyabalaji2000 <85394915+chaitanyabalaji2000@users.noreply.github.com>
2021-06-05 17:40:58 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React.js</h1>
+      <p>
+      Shape AI is a greate way to learn basics of JavaScript and React.js
+      which were taught by industry experts having years of experience
+      in their respective fields. And Having cracked various top
+      MNC's in the world.
+      They where starting some new internship programs which will help students
+      a lot.
+
+ 
+        
+      </p>
+    </div>
+  );
+}
+
+export default Note;
